e2ec8ab961f8023cb90ecf36e2c7dd811c93c6c1
kenbomtom <kenbomtom@gmail.com>
2022-05-27 01:26:01 +0000

    hkde keys.ts

--- src/utils/getUserFromToken.ts
+++ src/utils/getUserFromToken.ts
@@ -1,13 +1,13 @@
 import JWT from "jsonwebtoken"
-import { JSON_SIGNATURE } from "../keys"
+// import { JSON_SIGNATURE } from "../keys"
 require('dotenv').config()
 const env = process.env
-// const JSON_SIGNATURE:any = env.JSON_SIGNATURE
+const JSON_SIGNATURE:any = env.JSON_SIGNATURE
 
 export const getUserFromToken = (token: string) => {
     console.log(`aferReceiveFromIndex:${token}`)
-    console.log(`checkEnv:${env.DATABASE_URL}`)
-    console.log(`checkEnv:${env.JSON_SIGNATURE}`)
+    // console.log(`checkEnv:${env.DATABASE_URL}`)
+    // console.log(`checkEnv:${env.JSON_SIGNATURE}`)
     try {
         return JWT.verify(token, JSON_SIGNATURE) as {
             userId: number
